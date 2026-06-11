a6fc0c77bf908816eb8a6539f2815c84f9cae9c5
congktx <buicongvip@gmail.com>
2026-06-12 02:50:07 +0700

    add reward api

--- src/modules/rewards/rewards.module.ts
+++ src/modules/rewards/rewards.module.ts
@@ -6,9 +6,10 @@ import { User } from '../users/entities/user.entity';
 import { RewardProof } from './entities/reward_proof.entity';
 import { RewardAppeal } from './entities/reward_appeal.entity';
 import { RewardRule } from './entities/reward_rule.entity';
+import { Wallet } from '../wallets/entities/wallet.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, RewardProof, RewardAppeal, RewardRule])],
+  imports: [TypeOrmModule.forFeature([User, Wallet, RewardProof, RewardAppeal, RewardRule])],
   controllers: [RewardsController],
   providers: [RewardsService],
   exports: [RewardsService, TypeOrmModule],
